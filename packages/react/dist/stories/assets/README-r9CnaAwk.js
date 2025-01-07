083f99ed7ad3b4b848bc306d92c3cd6a70e77310
AwesomeDevin <awesomedevin.deng@gmail.com>
2025-01-07 12:03:32 +0800

    update

---
+++ packages/react/dist/stories/assets/README-r9CnaAwk.js
@@ -0,0 +1,60 @@
+import{R as n}from"./banner-D5Q7cPnP.js";import{bn as l,R as o}from"./index2-BaOVXFcn.js";import"./index-CntKg-mK.js";const c={},e={...o},p=e.createElement;var r={wrap:null};e.createElement=(d,...a)=>{const t=r.wrap;let i=d;return Object.values(c).includes(i)&&t&&(i=t(d,{React:o})),p(i,...a)};const m=(d,a)=>{try{a.wrap&&(r.wrap=a.wrap)}catch{}};function v(d){l.render(e.createElement("div",{style:{display:"flex",gap:10,overflowX:"auto"}},e.createElement("div",null,e.createElement("div",null,"@banner-responsive/react :"),e.createElement("div",{style:{display:"flex",flexDirection:"column",border:"1px solid #e5eafe",borderRadius:18,width:1e3,height:750,overflow:"hidden"}},e.createElement(n,{backgroundPosition:"top",img:"https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png"}))),e.createElement("div",null,e.createElement("div",null,'"div" bg mode - cover :'),e.createElement("div",{style:{display:"flex",flexDirection:"column",border:"1px solid #e5eafe",borderRadius:18,width:1e3,height:750,background:"url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top",backgroundSize:"cover"}})),e.createElement("div",null,e.createElement("div",null,'"div" bg mode - contain :'),e.createElement("div",{style:{display:"flex",flexDirection:"column",border:"1px solid #e5eafe",borderRadius:18,width:1e3,height:750,background:"url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top",backgroundSize:"contain"}}))),d)}const u=!0,b={value:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          overflow: 'hidden'
+        }}
+      >
+    <Banner backgroundPosition="top" img="https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png" />
+    </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top",
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`};export{u as IS_FN_DEMO$,b as content$,v as default,m as setWrap$};
---
+++ packages/react/dist/stories/assets/README-zJTPqHGv.js
@@ -0,0 +1,297 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/README-BBJUZxie.js","assets/banner-D5Q7cPnP.js","assets/index2-BaOVXFcn.js","assets/index-CntKg-mK.js","assets/B4Ixr1Fe.css","assets/README-Ct27OcjR.js","assets/README-DAfzTEJE.js","assets/README-r9CnaAwk.js","assets/README-BCBKwBb9.js"])))=>i.map(i=>d[i]);
+import{_ as d}from"./index-CntKg-mK.js";import{R as e,h as i,v as r}from"./index2-BaOVXFcn.js";const t={filename:"/Users/dengwen/banner-responsive/packages/react/src/README.md",pathHash:"_2f3bb0917fb877c59354c44eaaf5d03e",frontmatter:{title:"ResponsiveBanner",toc:"menu",filename:"src/README.md",lastUpdated:1736219997e3,order:0},demos:[{id:"src-readme-demo-0",style:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          overflow: 'hidden'
+        }}
+      >
+        <Banner backgroundPosition="center" img="https://img.lazcdn.com/g/gcp/lazada/caab1d56-6f25-4449-9ab5-277729c2d991_SG-3000-1524.jpg_2200x2200q80.jpg" />
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/caab1d56-6f25-4449-9ab5-277729c2d991_SG-3000-1524.jpg_2200x2200q80.jpg') no-repeat center" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/caab1d56-6f25-4449-9ab5-277729c2d991_SG-3000-1524.jpg_2200x2200q80.jpg') no-repeat center" ,
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`,load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-0",load:()=>d(()=>import("./README-BBJUZxie.js"),__vite__mapDeps([0,1,2,3,4]))}},{id:"src-readme-demo-1",style:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          overflow: 'hidden'
+        }}
+      >
+    <Banner backgroundPosition="right" img="https://img.lazcdn.com/g/gcp/lazada/3384ee66-6160-409e-aad0-174e031c0cd9_SG-8001-3750.jpg_2200x2200q80.jpg" />
+   </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/3384ee66-6160-409e-aad0-174e031c0cd9_SG-8001-3750.jpg_2200x2200q80.jpg') no-repeat right" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/3384ee66-6160-409e-aad0-174e031c0cd9_SG-8001-3750.jpg_2200x2200q80.jpg') no-repeat right",
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`,load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-1",load:()=>d(()=>import("./README-Ct27OcjR.js"),__vite__mapDeps([5,1,2,3,4]))}},{id:"src-readme-demo-2",style:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          overflow: 'hidden'
+        }}
+      >
+    <Banner backgroundPosition="left" img="https://img.lazcdn.com/g/tps/imgextra/i1/O1CN01x0Y38j22hDHsu3cgK_!!6000000007151-0-tps-2400-1215.jpg_2200x2200q80.jpg" />
+   </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/tps/imgextra/i1/O1CN01x0Y38j22hDHsu3cgK_!!6000000007151-0-tps-2400-1215.jpg_2200x2200q80.jpg') no-repeat left" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 350,
+          background: "url('https://img.lazcdn.com/g/tps/imgextra/i1/O1CN01x0Y38j22hDHsu3cgK_!!6000000007151-0-tps-2400-1215.jpg_2200x2200q80.jpg') no-repeat left",
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`,load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-2",load:()=>d(()=>import("./README-DAfzTEJE.js"),__vite__mapDeps([6,1,2,3,4]))}},{id:"src-readme-demo-3",style:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          overflow: 'hidden'
+        }}
+      >
+    <Banner backgroundPosition="top" img="https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png" />
+    </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat top",
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`,load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-3",load:()=>d(()=>import("./README-r9CnaAwk.js"),__vite__mapDeps([7,1,2,3,4]))}},{id:"src-readme-demo-4",style:`import Banner from '@banner-responsive/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ <div style={{display: 'flex', gap: 10, overflowX: 'auto' }}>
+      <div>
+        <div>@banner-responsive/react :</div>
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          overflow: 'hidden'
+        }}
+      >
+    <Banner backgroundPosition="bottom" img="https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png" />
+    </div>
+    </div>
+    <div>
+      <div>"div" bg mode - cover :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat bottom" ,
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    </div>
+    <div>
+      <div>"div" bg mode - contain :</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          
+          border: '1px solid #e5eafe',
+          borderRadius: 18,
+          width: 1000,
+          height: 750,
+          background: "url('https://img.lazcdn.com/g/gcp/lazada/d6c294a7-f228-4be7-8fff-5d3af62d78d9_SG-2400-1215.png_2200x2200q80.png') no-repeat bottom",
+          backgroundSize: 'contain'
+        }}
+      >
+      </div>
+    </div>
+  </div>,
+  mountNode,
+);`,load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-4",load:()=>d(()=>import("./README-BCBKwBb9.js"),__vite__mapDeps([8,1,2,3,4]))}}]};function c(){return e.createElement(e.Fragment,null,e.createElement(i,{type:"cardBlock"},e.createElement("h1",{id:"effect-of-diff---center"},e.createElement("a",{"aria-hidden":"true",tabIndex:"-1",href:"#effect-of-diff---center"},e.createElement("span",{className:"icon icon-link"})),"Effect of Diff - center"),e.createElement(r,{demo:{id:"src-readme-demo-0"},previewerProps:{},load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-0",load:()=>d(()=>import("./README-BBJUZxie.js"),__vite__mapDeps([0,1,2,3,4]))}})),e.createElement(i,{type:"cardBlock"},e.createElement("h1",{id:"effect-of-diff---right"},e.createElement("a",{"aria-hidden":"true",tabIndex:"-1",href:"#effect-of-diff---right"},e.createElement("span",{className:"icon icon-link"})),"Effect of Diff - right"),e.createElement(r,{demo:{id:"src-readme-demo-1"},previewerProps:{},load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-1",load:()=>d(()=>import("./README-Ct27OcjR.js"),__vite__mapDeps([5,1,2,3,4]))}})),e.createElement(i,{type:"cardBlock"},e.createElement("h1",{id:"effect-of-diff---left"},e.createElement("a",{"aria-hidden":"true",tabIndex:"-1",href:"#effect-of-diff---left"},e.createElement("span",{className:"icon icon-link"})),"Effect of Diff - left"),e.createElement(r,{demo:{id:"src-readme-demo-2"},previewerProps:{},load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-2",load:()=>d(()=>import("./README-DAfzTEJE.js"),__vite__mapDeps([6,1,2,3,4]))}})),e.createElement(i,{type:"cardBlock"},e.createElement("h1",{id:"effect-of-diff---top"},e.createElement("a",{"aria-hidden":"true",tabIndex:"-1",href:"#effect-of-diff---top"},e.createElement("span",{className:"icon icon-link"})),"Effect of Diff - top"),e.createElement(r,{demo:{id:"src-readme-demo-3"},previewerProps:{},load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-3",load:()=>d(()=>import("./README-r9CnaAwk.js"),__vite__mapDeps([7,1,2,3,4]))}})),e.createElement(i,{type:"cardBlock"},e.createElement("h1",{id:"effect-of-diff---bottom"},e.createElement("a",{"aria-hidden":"true",tabIndex:"-1",href:"#effect-of-diff---bottom"},e.createElement("span",{className:"icon icon-link"})),"Effect of Diff - bottom"),e.createElement(r,{demo:{id:"src-readme-demo-4"},previewerProps:{},load:{id:"/src/README.md?markdown-proxy&id=src-readme-demo-4",load:()=>d(()=>import("./README-BCBKwBb9.js"),__vite__mapDeps([8,1,2,3,4]))}})))}export{c as default,t as meta$};
